402c08c079be7f06db60e9b449d277cda39af484
maxime dupouy <maxime_dupouy@hotmail.fr>
2022-12-05 17:28:11 +0100

    minor changes config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ MODEL_PATH = "./models/"
 MODEL_TO_USE = "random_forest_final.pkl"
 #MODEL_TO_USE = "random_orest_no_finetuning.pkl"
 DATA_PATH = './data/df_complete.csv.gz'
-PORT = '8000'
 HOST = 'http://127.0.0.1:'
+HEROKU_HOST = 'scoring-credit-oc-48975'
 EMPTY_RESPONSE = {
     'error': {'status' : None,
               'client_id_sample' : None,
